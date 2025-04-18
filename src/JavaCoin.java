9539186a9108c18e65faefadf030456f6fe3db9f
Oskar Karimov <oskarkarimovoskar@gmail.com>
2025-04-18 23:55:04 +0200

    Add wallet and blockchain transaction signing

--- src/JavaCoin.java
+++ src/JavaCoin.java
@@ -1,22 +1,23 @@
 import java.util.Date;
+import java.util.ArrayList;
 
 public class JavaCoin {
     public String hash;
     public String previousHash;
-    private String data; //data will be a simple message
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
     private long timeStamp; //as number of milliseconds
     private int nonce;
 
     //Block Constructor
-    public JavaCoin(String data, String previousHash) {
-        this.data = data;
+    public JavaCoin(String previousHash) {
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
-        this.hash = calculateHash(); //Making sure we do this after we set the other values
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
     }
 
     public String calculateHash() {
-        return StringUtil.applySha(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
+        return StringUtil.applySha(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + merkleRoot);
     }
 
     public void mineBlock(int difficulty) {
@@ -26,5 +27,21 @@ public class JavaCoin {
             hash = calculateHash();
         }
         System.out.println("Block Mined :D -> " + hash);
+    }
+
+    //Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((!"0".equals(previousHash))) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
     }
 }
---
+++ src/Transaction.java
@@ -0,0 +1,99 @@
+import java.security.*;
+import java.util.ArrayList;
+
+public class Transaction {
+    public String transactionId; // hash of the transaction
+    public PublicKey sender; //sender address/public key
+    public PublicKey reciepient; //recipients address/public key
+    public float value;
+    public byte[] signature; //to prevent anybody else from spending funds in our wallet
+
+    public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+    public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+
+    private static int sequence = 0; // a rough count of how many transactions have been generated.
+
+    // Constructor:
+    public Transaction(PublicKey from, PublicKey to, float value,  ArrayList<TransactionInput> inputs) {
+        this.sender = from;
+        this.reciepient = to;
+        this.value = value;
+        this.inputs = inputs;
+    }
+
+    // This Calculates the transaction hash (which will be used as its Id)
+    public boolean processTransaction() {
+
+        if(verifySignature() == false) {
+            System.out.println("#Transaction Signature failed to verify");
+            return false;
+        }
+
+        //Gathers transaction inputs (Making sure they are unspent):
+        for(TransactionInput i : inputs) {
+            i.UTXO = Main.UTXOs.get(i.transactionOutputId);
+        }
+
+        //Checks if transaction is valid:
+        if(getInputsValue() < Main.minimumTransaction) {
+            System.out.println("Transaction Inputs too small: " + getInputsValue());
+            System.out.println("Please enter the amount greater than " + Main.minimumTransaction);
+            return false;
+        }
+
+        //Generate transaction outputs:
+        float leftOver = getInputsValue() - value; //get value of inputs then the left over change:
+        transactionId = calulateHash();
+        outputs.add(new TransactionOutput( this.reciepient, value,transactionId)); //send value to recipient
+        outputs.add(new TransactionOutput( this.sender, leftOver,transactionId)); //send the left over 'change' back to sender
+
+        //Add outputs to Unspent list
+        for(TransactionOutput o : outputs) {
+            Main.UTXOs.put(o.id , o);
+        }
+
+        //Remove transaction inputs from UTXO lists as spent:
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it
+            Main.UTXOs.remove(i.UTXO.id);
+        }
+
+        return true;
+    }
+
+    public float getInputsValue() {
+        float total = 0;
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it, This behavior may not be optimal.
+            total += i.UTXO.value;
+        }
+        return total;
+    }
+
+    public void generateSignature(PrivateKey privateKey) {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+        signature = StringUtil.applyECDSASig(privateKey,data);
+    }
+
+    public boolean verifySignature() {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+        return StringUtil.verifyECDSASig(sender, data, signature);
+    }
+
+    public float getOutputsValue() {
+        float total = 0;
+        for(TransactionOutput o : outputs) {
+            total += o.value;
+        }
+        return total;
+    }
+
+    private String calulateHash() {
+        sequence++; //increase the sequence to avoid 2 identical transactions having the same hash
+        return StringUtil.applySha(
+                StringUtil.getStringFromKey(sender) +
+                        StringUtil.getStringFromKey(reciepient) +
+                        Float.toString(value) + sequence
+        );
+    }
+}
